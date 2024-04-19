f63a9f6ff1f625772b4cb2b2566730de4e55d3d6
IrfanShahab <ahmad.heads2002@gmail.com>
2024-04-19 03:56:34 -0700

    Button In HoePage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_application/screens/user_data.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => UserDetails()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
